9024299f61931e4802ddc81461ffe0725d08d0b5
Kevin Tong <logicmd@gmail.com>
2014-06-03 17:48:30 +0800

    refactor

--- src/main/java/us/hk/bdwm/api/controller/PostController.java
+++ src/main/java/us/hk/bdwm/api/controller/PostController.java
@@ -1,17 +1,45 @@
 package us.hk.bdwm.api.controller;
 
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpMethod;
+import org.apache.commons.httpclient.methods.GetMethod;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 import us.hk.bdwm.api.core.Post;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Controller
 public class PostController {
     private static final Logger LOG = LoggerFactory
             .getLogger(PostController.class);
+
+    private static final String postUrlPrefix = "http://www.bdwm.net/bbs/bbscon.php?";
+
+    private HttpClient client;
+
+    private DocumentBuilder docBuilder;
+
+    public PostController() throws ParserConfigurationException {
+        client = new HttpClient();
+        client.getHostConfiguration().setProxy("proxy.logicmd.net", 8484);
+        DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
+        docBuilder = docBuilderFactory.newDocumentBuilder();
+    }
 
     @RequestMapping("/post")
     public
@@ -22,6 +50,36 @@ public class PostController {
             @RequestParam(value = "num", required = true) String num,
             @RequestParam(value = "attach", required = false, defaultValue = "0") String attach,
             @RequestParam(value = "dig", required = false, defaultValue = "0") String dig) {
-        return new Post(board, file, num, Integer.parseInt(attach), Integer.parseInt(dig));
+
+        String rawContent = null;
+        String url = postUrlPrefix + board + "/" + file + "/" + num + "/" + attach + "/" + dig;
+
+        HttpMethod method = new GetMethod(url);
+        try {
+            client.executeMethod(method);
+
+            if (method.getStatusCode() == 200) {
+                String body = method.getResponseBodyAsString();
+
+                Document doc = null;
+
+                doc = docBuilder.parse(new InputSource(new ByteArrayInputStream(body.getBytes())));
+
+
+                rawContent = doc.getElementsByTagName("pre").item(0).getNodeValue();
+                //Pattern p = Pattern.compile("<pre>(.*?)<\\/pre>", Pattern.DOTALL);
+                //Matcher m = p.matcher(body);
+
+//                if (m.find()) {
+//                    rawContent = m.group(1);
+//                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        }
+
+        return new Post(rawContent);
     }
 }
--- src/main/java/us/hk/bdwm/api/core/Post.java
+++ src/main/java/us/hk/bdwm/api/core/Post.java
@@ -1,55 +1,24 @@
 package us.hk.bdwm.api.core;
 
-import javax.annotation.Resource;
-
 public class Post {
 
-    public static final String postUrlPrefix = "http://www.bdwm.net/bbs/bbscon.php";
+    public static final String postUrlPrefix = "http://www.bdwm.net/bbs/bbscon.php?";
 
-    private String board;
-
-    private String file;
-
-    private String num;
-
-    private int attach;
-
-    private int dig;
+    private String author;
 
     private String content;
 
-    public Post(String board, String file, String num, int attach, int dig) {
-        this.board = board;
-        this.file = file;
-        this.num = num;
-        this.attach = attach;
-        this.dig = dig;
-        getContent();
+    public Post(String rawContent) {
+        author = "在这里Parse Author";
+        content = "在这里Parse 帖子内容";
     }
 
-    public String getBoard() {
-        return board;
-    }
-
-    public String getFile() {
-        return file;
-    }
-
-    public String getNum() {
-        return num;
-    }
-
-    public int getAttach() {
-        return attach;
-    }
-
-    public int getDig() {
-        return dig;
+    public String getAuthor() {
+        return author;
     }
 
     public String getContent() {
-        if (content == null)
-            content = postUrlPrefix + "?board=" + board + "&file=" + file + "&num=" + num + "&dig=" + dig;
         return content;
     }
+
 }
